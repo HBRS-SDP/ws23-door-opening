0837858ee43a4426c1c778749b68c8eaf739e3a4
Maira Liaquat <mairaliaqat317@gmail.com>
2024-02-10 09:33:46 +0100

    adding publisher

--- ros_pkg/lever_detection/scripts/lever.py
+++ ros_pkg/lever_detection/scripts/lever.py
@@ -57,11 +57,7 @@ class LeverDetector:
             rospy.logwarn("Image or point cloud data not yet available.")
             return
 
-        # Process image with model
-        # results = self.model(self.cv_image)
-        # detections = results.xyxy[0]  # Assuming you want all detections. Filter as needed.
-
-
+      
         results = self.model(self.cv_image, size=416)
         self.publish_image(results.render()[0])
         print(results.pandas().xyxy[0])
@@ -109,17 +105,20 @@ class LeverDetector:
                 print(obj_pose)
                 print("-------------------------------")
 
-                real_object_head = self.transform_3D2head([obj_pose])[0]
+                real_object_camera = self.transform_3D2camera([obj_pose])[0]
                 print("-----------real_object_head-------------------")
-                print(real_object_head)
+                print(real_object_camera)
                 print("-------------------------------")
-                #real_object_pose = self.mover.transform_head2map([real_object_head])[0]
-                real_object_pose = self.transform_head2base([real_object_head])[0]
-                print("-----------real_object_pose-------------------")
-                print(real_object_pose)
-                print("-------------------------------")
-                # final_pose = real_object_pose ## use this one for realtime working
-                final_pose = real_object_head ## this is for test only
+                final_pose = real_object_camera ## this is for test only
+
+
+                # real_object_base = self.transform_camera2base([real_object_camera])[0]  ## call this function to tranform pose from camera_link to desired
+                # print("-----------real_object_pose-------------------")
+                # print(real_object_base)
+                # print("-------------------------------")
+                # final_pose = real_object_base ## use this one for realtime working
+                
+                self.pose_pub.publish(final_pose)
                 break
             except Exception as e:
                 print(e)
@@ -128,7 +127,7 @@ class LeverDetector:
         if final_pose==None:
             return 'failed'
 
-        print(real_object_head)
+        print(real_object_camera)
     def show_point(self, point, col=[1,1,1]):
             pcd = o3d.geometry.PointCloud()
             pcd.points = o3d.utility.Vector3dVector([point])
@@ -170,7 +169,7 @@ class LeverDetector:
         except ValueError as e:
             print(e)
 
-    def transform_3D2head(self,obj_poses):
+    def transform_3D2camera(self,obj_poses):
         transformed = []
         for obj_pose in obj_poses:
             x,y,z = obj_pose.pose.position.x, obj_pose.pose.position.y, obj_pose.pose.position.z
@@ -188,7 +187,7 @@ class LeverDetector:
     #         transformed.append(transformed_pose)
     #     return transformed
 
-    def transform_head2base(self,obj_poses):
+    def transform_camera2base(self,obj_poses):
         transformed = []
         for obj_pose in obj_poses:
             transformation = self.tfbuffer_.lookup_transform('BASE', "camera_link", rospy.Time())
