7ab666440dc57190f63d3f3584fe478422fb8694
Maira Liaquat <mairaliaqat317@gmail.com>
2024-02-12 22:54:47 +0100

    refactored code

--- src/lever_detection/scripts/doorz.py
+++ src/lever_detection/scripts/doorz.py
@@ -8,28 +8,28 @@ from cv_bridge import CvBridge
 from std_msgs.msg import String  # Simple string message for demonstration
 
 # Load a pre-trained object detection model. Adjust path as necessary.
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='src/lever_detection/model/door.pt')
+object_detection_model = torch.hub.load('ultralytics/yolov5', 'custom', path='src/lever_detection/model/door.pt')
 bridge = CvBridge()
 
-def detect_lever_and_draw_boxes(image):
+def detect_objects_and_draw_boxes(image):
     # Detect objects in the image using the loaded model
-    result = model(image, size=416)
+    result = object_detection_model(image, size=416)
     detection_results = result.pandas().xyxy[0]  # Extract detection results
 
     # Filter detection results for levers (assuming class '2' is for levers, adjust as necessary)
     lever_detections = detection_results[detection_results['class'] == 2]
 
     # Initialize an empty string to accumulate bounding box information
-    detected_levers_str = ""
+    detected_objects_str = ""
 
-    # Process detected levers and prepare bounding box information
+    # Process detected objects and prepare bounding box information
     for index, row in lever_detections.iterrows():
         xmin, ymin, xmax, ymax = int(row['xmin']), int(row['ymin']), int(row['xmax']), int(row['ymax'])
         confidence = row['confidence']
-        detected_levers_str += f"Lever detected with confidence {confidence:.2f} at [{xmin}, {ymin}, {xmax}, {ymax}]\n"
+        detected_objects_str += f"Lever detected with confidence {confidence:.2f} at [{xmin}, {ymin}, {xmax}, {ymax}]\n"
 
-    # Return the string representation of detected levers
-    return detected_levers_str
+    # Return the string representation of detected objects
+    return detected_objects_str
 
 def image_callback(msg):
     try:
@@ -37,17 +37,17 @@ def image_callback(msg):
     except CvBridgeError as e:
         print(e)
 
-    detected_levers_str = detect_lever_and_draw_boxes(cv_image)
+    detected_objects_str = detect_objects_and_draw_boxes(cv_image)
 
-    # Publish the detected levers
-    if detected_levers_str:
-        bbox_pub.publish(detected_levers_str)
+    # Publish the detected objects
+    if detected_objects_str:
+        objects_pub.publish(detected_objects_str)
     else:
-        bbox_pub.publish("No levers detected")
+        objects_pub.publish("No levers detected")
 
 if __name__ == '__main__':
-    rospy.init_node('lever_detector_node')
+    rospy.init_node('object_detector_node')
     rospy.Subscriber("/image_raw", Image, image_callback)
-    bbox_pub = rospy.Publisher("/detected_levers", String, queue_size=10)
+    objects_pub = rospy.Publisher("/detected_objects", String, queue_size=10)
 
     rospy.spin()
